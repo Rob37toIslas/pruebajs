1b32c480841c338ec6d36857f49f2a276a47c7ea
Robert CI <97404217+Rob37toIslas@users.noreply.github.com>
2026-03-25 10:58:54 -0600

    Update webhook URL and clean up comments

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const port = 3000;
 const verifyToken = "MiTokenDeVerificacion123!";
 
 // ⭐ URL de tu webhook de n8n
-const N8N_WEBHOOK_URL = 'https://juanjoetm123.app.n8n.cloud/webhook-test/37968c75-e396-4d78-837f-99ca666293a7';
+const N8N_WEBHOOK_URL = 'https://n8n-2-9c9v.onrender.com/webhook/324a36ef-7110-4878-b085-9583d5d89c4d';
 
 // Route for GET requests
 app.get('/', (req, res) => {
